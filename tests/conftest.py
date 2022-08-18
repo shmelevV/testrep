af51eacf939cebc007d274289260a313572a2314
shmelevV <96441389+shmelevV@users.noreply.github.com>
2022-08-18 17:13:34 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,12 +19,11 @@ from configs.db_parser import delete_group, delete_user, delete_auth_user_group
 
 @pytest.fixture(scope='class')
 def browser():
+def browser():
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-infobars')
-    chrome_options.add_argument('--remote-debugging-port=9222')
     driver = webdriver.Chrome("/usr/local/bin/chromedriver", options=chrome_options)
     driver.maximize_window()
     driver.implicitly_wait(10)
