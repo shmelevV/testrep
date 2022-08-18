158fd36ea30ef789f58544bd9c80cda5521de6cf
shmelevV <96441389+shmelevV@users.noreply.github.com>
2022-08-18 15:19:53 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,8 +19,8 @@ from configs.db_parser import delete_group, delete_user, delete_auth_user_group
 @pytest.fixture(scope='class')
 def browser():
     chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
